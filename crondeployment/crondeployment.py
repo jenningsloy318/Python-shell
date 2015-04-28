075d1a33acbc17adb1d8afd4a079b0317bd4eb54
jenningsloy318 <1641626342@qq.com>
2015-04-28 21:26:50 +0800

    add some key features of the crondeployment

--- crondeployment/crondeployment.py
+++ crondeployment/crondeployment.py
@@ -13,12 +13,23 @@ def main():
     global log_file,cmd_list,user,passwd
     arguments = argparse.ArgumentParser()  
     arguments.add_argument("-c","--cvs_list",nargs="?",help="The cvs  list")
-    arguments.add_argument("-l","--log", nargs="?",help="The log file,default result.log",default="result.log")
+    arguments.add_argument("-S","--server_name",nargs="?",help="us_cron/us_cron1/ukcron/xdcron/cacron/hyve_cron/hyve_cron1/mxcron/gis")
+    arguments.add_argument("-D","--deploy_plan_id",nargs="?",help="deploy plan ID")
     if len(sys.argv) ==1:
         arguments.print_help()
         sys.exit(1)
     args = arguments.parse_args()
     cvs_list = open(args.cvs_list)
+    判断是输入的server，然后链接数据库查询是否有DP，及是否有cron的DO，有的话就列出来，看选择是否部署
+    判断输入的是DP， 然后链接数据库查询该DP所在的server和DO 进行确认
+
+    根据server和DP，查询出相关cvs路径和实际路径进行checkout操作。
+    循环操作，询问是否还要继续进行部署。
+
+    server = args.server
+    mysqli=.....
+
+    dp
     for cvs_cmd in cvs_list.readline
 
     log_file =args.log
