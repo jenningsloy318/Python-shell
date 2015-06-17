91d6295bba652d891a866c7da8803841aa5bba4b
JenningsL <jenningsloy318@gmail.com>
2015-06-17 11:14:48 +0800

    modify lambda comments

--- basic/function_sorted.py
+++ basic/function_sorted.py
@@ -3,7 +3,7 @@
 # Jennings Liu@ 2015-06-17 10:08:11
 
 sort_tuple=[('acbd',15,'male'),('ccc',22,'female'),('dfd',17,'male')]
-sort_result=sorted(sort_tuple,key=lambda student: student[1]) #lambda 定义一个单行函数.
+sort_result=sorted(sort_tuple,key=lambda student: student[1]) #lambda 定义一个单行函数, lambda student:student[1],此时student是它的参数
 print(sort_result)
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 L_result1=sorted(L,key=lambda scores:scores[1],reverse=True)
