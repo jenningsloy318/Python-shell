1b3a70fe3059388de1ac63d252e68e84e2f480a1
JenningsL <jenningsloy318@gmail.com>
2015-11-03 17:48:05 +0800

    remove the device from zenoss

---
+++ zenoss/remove_device_by_uid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+from devices import zenoss_session
+
+ZENOSS_INSTANCE = 'http://10.88.126.71:8080'
+ZENOSS_USERNAME = 'admin'
+ZENOSS_PASSWORD = 'passwd'
+
+ROUTERS = { 'MessagingRouter': 'messaging',
+            'EventsRouter': 'evconsole',
+            'ProcessRouter': 'process',
+            'ServiceRouter': 'service',
+            'DeviceRouter': 'device',
+            'NetworkRouter': 'network',
+            'TemplateRouter': 'template',
+            'DetailNavRouter': 'detailnav',
+            'ReportRouter': 'report',
+            'MibRouter': 'mib',
+            'ZenPackRouter': 'zenpack' }
+
+newsession=zenoss_session(ZENOSS_USERNAME,ZENOSS_PASSWORD,ZENOSS_INSTANCE,ROUTERS)
+host_list='hosts.list'
+for line in open(host_list):
+    (IP,hostname,group)=line.split(",",3)
+    uid='/zport/dmd/Devices/Server/Linux/devices/'+IP
+    z=newsession.router_request('DeviceRouter', 'removeDevices',
+        data=[{'uids':uid,'hashcheck':'','action':'delete'}])['result']
+
+
+
+print z
