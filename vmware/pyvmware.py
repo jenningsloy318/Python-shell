868cde724d7e3705dfba2a4e5e224ec7f6525526
jenningsloy318 <jenningsloy318@gmail.com>
2016-07-16 14:30:56 +0800

    add condition fo pyvmomi checking and other enhancements

--- vmware/pyvmware.py
+++ vmware/pyvmware.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Jennings Liu@ 2015-07-21 14:32:07
 
@@ -11,18 +11,75 @@ import argparse
 import getpass
 import sys
 import ssl
+import pkg_resources
 
 class pyvmware(object):
     def __init__(self):
         ssl._create_default_https_context = ssl._create_unverified_context
+<<<<<<< HEAD
+        if pkg_resources.get_distribution("pyVmomi").version.startswith('6'):
+            self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+            self.context.verify_mode=ssl.CERT_NONE
+            requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+        else:
+            requests.packages.urllib3.disable_warnings()
+=======
         default_context = ssl._create_default_https_context
         default_context.verify_mode=ssl.CERT_NONE
         requests.packages.urllib3.disable_warnings()
+>>>>>>> 75ebb16150d47a20a514869fc5a02d5b2739d1bd
 
     def login(self,host,user,passwd):
         self.host=host
         self.user=user
         self.passwd=passwd
+<<<<<<< HEAD
+        if hasattr(self,'context'):
+            self.connection=SmartConnect(host=self.host,user=self.user,pwd=self.passwd,sslContext=self.context)
+        else:
+            self.connection=SmartConnect(host=self.host,user=self.user,pwd=self.passwd)
+        self.content=self.connection.RetrieveContent()
+        self.VCrootFolder=self.content.rootFolder
+    def get_dcs(self):
+        Datacenters={}
+        for dc in self.VCrootFolder.childEntity:
+            Datacenters[dc.name]=dc
+        return Datacenters
+
+
+    def get_all_hostFolders(self):
+        hostFolders={}
+        for dc in self.get_dcs().values():
+            hostFolders[dc.hostFolder.name]=dc.hostFolder
+        return hostFolders
+
+    def get_hostFolder_per_dc(self,dc):
+        hostFolders={}
+        hostFolders[dc.hostFolder.name]=dc.hostFolder
+        return hostFolders
+
+    def get_clusters_per_dc(self,dc):
+        clusters={}
+        for hostfolder in self.get_hostFolder_per_dc(dc).values():
+            for cluster in hostfolder.childEntity:
+                clusters[cluster.name]=cluster
+        return clusters
+
+
+    def get_datastores_per_dc(self,dc):
+        datastores={}
+        for datastore in dc.datastore:
+            datastores[datastore.name]=datastore
+        return datastores
+
+    def get_datastores_per_cluster(self,cluster):
+        datastores={}
+        for datastore in cluster.datastore:
+            datastores[datastore.name]=datastore
+        return datastores
+
+    def get_top_resourcepool_per_cluster(self,cluster):
+=======
         self.connection=SmartConnect(host=self.host,user=self.user,pwd=self.passwd)
         self.content=self.connection.RetrieveContent()
         self.viewManager=self.content.viewManager
@@ -50,15 +107,24 @@ class pyvmware(object):
         return clusters
 
     def get_cluster_top_resourcepool(self,cluster):
+>>>>>>> 75ebb16150d47a20a514869fc5a02d5b2739d1bd
         cluster_top_resourcepools={}
         cluster_top_resourcepools[cluster.name]=cluster.resourcePool
         return cluster_top_resourcepools
 
+<<<<<<< HEAD
+    def get_top_resourcepools_per_dc(self,dc):
+        dc_top_resourcepools={}
+        for cluster in self.get_clusters_per_dc(dc).values():
+            dc_top_resourcepools[cluster.name]=cluster.resourcePool
+        return dc_top_resourcepools
+=======
     def get_all_top_resourcepools(self):
         all_top_resourcepools={}
         for cluster in self.get_clusters().values():
             all_top_resourcepools[cluster.name]=cluster.resourcePool
         return all_top_resourcepools
+>>>>>>> 75ebb16150d47a20a514869fc5a02d5b2739d1bd
 
     def get_sub_resourcepools(self,resourcepool):
         sub_resourcepools={}
@@ -66,6 +132,37 @@ class pyvmware(object):
             sub_resourcepools[sub_resourcepool.name]=sub_resourcepool
         return sub_resourcepools
 
+<<<<<<< HEAD
+
+    def get_hosts_per_cluster(self,cluster):
+        cluster_hosts={}
+        for host in cluster.host:
+            cluster_hosts[host.name]=host
+        return cluster_hosts
+
+    def get_hosts_per_dc(self,dc):
+        dc_hosts={}
+        for cluster in self.get_clusters_per_dc(dc).values():
+            for host in cluster.host:
+                dc_hosts[host.name]=host
+        return dc_hosts
+        
+
+    def get_vms_per_dc(self,dc):
+        vmdict={}
+        for top_resourcepool in self.get_top_resourcepools_per_dc(dc).values():
+            vmdict=self.get_vms_per_resourcepool(top_resourcepool,vmdict)
+        return vmdict
+
+    def get_vms_per_cluster(self,cluster):
+        vmdict={}
+        for top_resourcepool in self.get_top_resourcepool_per_cluster(cluster).values():
+            vmdict=self.get_vms_per_resourcepool(top_resourcepool,vmdict)
+        return vmdict
+        
+    def get_vms_per_resourcepool(self,resourcepool,vmdict):
+        if  self.get_sub_resourcepools(resourcepool).items() and hasattr(resourcepool,'vm'):
+=======
     def get_cluster_hosts(self,cluster):
         cluster_hosts={}
         self.hosts=cluster.host
@@ -91,10 +188,24 @@ class pyvmware(object):
 
     def get_resourecepool_vms(self,resourcepool,vmdict):
         if  hasattr(resourcepool,'resourcePool') and hasattr(resourcepool,'vm'):
+>>>>>>> 75ebb16150d47a20a514869fc5a02d5b2739d1bd
             for vm in resourcepool.vm:
                 vmdict[vm.name]=vm
             sub_resourcepool=resourcepool.resourcePool
             for c in sub_resourcepool:
+<<<<<<< HEAD
+                self.get_vms_per_resourcepool(c,vmdict)
+        elif  self.get_sub_resourcepools(resourcepool).items()  and not hasattr(resourcepool,'vm'):
+            sub_resourcepool=resourcepool.resourcePool
+            for c in sub_resourcepool:
+                self.get_vms_per_resourcepool(c,vmdict)
+        elif not  self.get_sub_resourcepools(resourcepool).items() and  hasattr(resourcepool,'vm'):
+            for vm in resourcepool.vm:
+                vmdict[vm.name]=vm
+        return vmdict
+
+
+=======
                 self.get_resourecepool_vms(c,vmdict)
         elif  hasattr(resourcepool,'resourcePool') and not hasattr(resourcepool,'vm'):
             sub_resourcepool=resourcepool.resourcePool
@@ -103,6 +214,7 @@ class pyvmware(object):
         elif not hasattr(resourcepool,'resourcePool') and  hasattr(resourcepool,'vm'):
             for vm in resourcepool.vm:
                 vmdict[vm.name]=vm
+>>>>>>> 75ebb16150d47a20a514869fc5a02d5b2739d1bd
 
     def get_vminfo(self,vm):
         vmsummary=vm.summary
@@ -127,20 +239,20 @@ class pyvmware(object):
             datastoresummary=datastore.summary
             freeSpace=datastoresummary.freeSpace
             capability=datastoresummary.capacity
-            if freeSpace is not None:
+            if freeSpace:
                 freeSpace=format(freeSpace/1014/1024/1024,'.0f')
-            if capability is not None:
+            if capability:
                 capability=format(capability/1014/1024/1024,'.0f')
             url=datastore.info.url
             name=datastore.name
             VMs=[]
             for vm in datastore.vm:
-                entry=str(vm.name)+':'+str(vm.summary.guest.ipAddress)
+                entry=':'.join([str(vm.name),str(vm.summary.guest.ipAddress)])
                 VMs.append(entry)  
-            if capability is not None and freeSpace is not None:
-                usage='{0:.2%}'.format(1-float(freeSpace)/float(capability),'.4f')
+            if capability  and freeSpace :
+                usage='{0:.2%}'.format(1-float(freeSpace)/float(capability))
                 used=str(usage)+' used'
-            elif freeSpace is None:
+            elif not freeSpace:
                 used='0%'
             else:
                 used='100%'
