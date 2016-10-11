5f57993056a065c48bd5a2764fd3f36ac9c0eadd
jenningsloy318 <jenningsloy318@gmail.com>
2016-10-11 14:51:00 +0800

    remove some useless characters

--- automount_nfs_flume/automount_nfs_flume.py
+++ automount_nfs_flume/automount_nfs_flume.py
@@ -7,10 +7,7 @@ import fcntl
 import struct
 import re
 import subprocess
-<<<<<<< HEAD
 import sys
-=======
->>>>>>> a09f613453dc4bf9a4b1110299ba5cb2baf91cca
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
@@ -19,7 +16,6 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-<<<<<<< HEAD
 def get_current_mount():
     cmd='mount|awk \'{if($5~/nfs/)print $0}\'|awk \'{print $1,$NF}\'|tr -d "(|)"| tr -s ":" " "|tr -s "=" " "| awk \'{print $1,$NF}\''
 #    curMount=subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
@@ -110,46 +106,3 @@ if __name__ == "__main__":
     for df_line in df_out:
         if  df_line:
 	    print(df_line)
-=======
-All_server_mount={'cron2.synnex.org':'/mnt/uscron2_spool','cron3.synnex.org':'/mnt/uscron3_spool','cron1.hyvesolutions.org':'/mnt/hycron1_spool','gis.synnex.org':'/mnt/edigis_spool','fca-vm-prod-esearch-index.synnex.org':'/mnt/esearch_spool','mxcron.synnex.org':'/mnt/mxcron_spool','cacron1.synnex.org':'/mnt/cacron1_spool','cron1-uk.synnex.org':'/mnt/ukcron1_spool','caedi.synnex.org':'/mnt/caedi_spool'}
-
-# regex expression to determine pattern
-LocalNet=('.').join(get_ip_address('eth0').split('.')[:2])
-if LocalNet=='10.88':
-    pattern=re.compile(r'10.88')
-elif LocalNet=='10.84':
-    pattern=re.compile(r'10.84|10.93|192.168.18')
-else:
-    pattern=re.compile('')
-##modify the dictionary according to the pattern
-for host in All_server_mount.keys():
-    #hostIP=socket.gethostbyname_ex(host)[2][0]
-    hostIP=socket.gethostbyname(host)
-    if not pattern.search(hostIP):
-        All_server_mount.pop(host)
-
-## umount and remount the nfs
-
-for server in All_server_mount.keys():
-    print('Umounting '+All_server_mount[server]+'!') 
-#    print('umount -l '+All_server_mount[server])
-    umount_out=subprocess.Popen('umount -l '+All_server_mount[server],shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-    if not umount_out[1]:
-    	print('Umount successfully '+All_server_mount[server]+umount_out[0]+'!') 
-    	print('Re-mounting the nfs share!')
-    	print('mount '+server+':/logs/flume '+All_server_mount[server])
-    	mount_out=subprocess.Popen('mount '+server+':/logs/flume '+All_server_mount[server],shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-    	if not mount_out[1]:
-    		print('Mount successfully '+mount_out[0]+'!\n') 
-    	else:
-    		print("Mount failed "+mount_out[0],mount_out[1],'!\n') 
-    else:
-	print("Umount "+All_server_mount[server]+" Failed, so will not re-mount it again!\n")
-
-
-
-
-df_out=subprocess.Popen('df -h',shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-for df_line in df_out:
-	print(df_line)
->>>>>>> a09f613453dc4bf9a4b1110299ba5cb2baf91cca
